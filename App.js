b0db54ed13645febfdc7a3241b993d48fee38c8d
aka55h-u <akashu97@gmail.com>
2025-02-11 10:48:25 +0530

    Component basics

--- App.js
+++ App.js
@@ -1,47 +1,57 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-const heading = React.createElement(
-    'h1', 
-    {id:"heading"}, 
-    "Hello from react");
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(heading);
 
+//jsx is not html inside javascript it is html like syntax
 
 /**
- * 
- *  <div id =parent>
- *      <div id="child">
- *          <h1> I am h1 </h1>
- *       </div>
- * </div>
- * 
- * 
- * 
+ * How jsx works
+ * JSX is not javascript for the engine to understand
+ * JSx is first transpiled by Babel to React.createElement which is then converted to JS Object which is rendered as HTML
  */
 
-const parent = React.createElement(
-    "div",
-    {id:"parent"},
-    React.createElement(
-        "div",
-        {id:"child"},
-        [React.createElement(
-            'h1',
-            {},
-            "I am a h1 tag"
-        ), React.createElement('h2',
-            {},
-            "I am a h2 tag"
-        ),
-        React.createElement('h3',
-            {},
-            "This is a h3 tag"
-        )]
-    )
+//giving attributes in jsx requires camelcase 
+//this is a react element
+const heading = (<h1 id="heading" className="head">This is heading from JSX</h1>);
+
+
+console.log(heading);
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(heading)
+
+//react based component 
+/**
+ * There are 2 wawys to create a react component 
+ *  FUnction based react component --> This is widely used in modern practises
+ * Class based react component 
+ */
+
+//react functional component - start with capital letter
+/**
+ *  function component is a js function that returns jsx or react element
+ *  
+ */
+
+const Title = () => (
+    <h1>This is the title of the component</h1>
+)
+
+const title2 = (
+    <h2>THis is a react element</h2>
+)
+//can put regular javascript inside jsx
+const num = 500;
+const HeadingComponent = () => (
+    <div id="container">
+        <Title />
+        {title2}
+        <h2>{num}</h2> 
+        <h1>Heading component </h1>
+
+    </div>
 );
 
-console.log(parent)
-const body = ReactDOM.createRoot(document.querySelector("body"));
-root.render(parent)
+//another way of writing
+const Heading2 = () => (<h1>Another functional compponent</h1>);
+
+root.render(< HeadingComponent />);
